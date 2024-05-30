5bc5b1099cdf7a764bcdca6d2e9d5d7de9e5dae2
SteveYaoLin <yaolin0616@163.com>
2024-05-30 22:05:21 +0800

    random_test extends uvm_test里面是关系，用到random_tester class，它继承自uvm_component。里面除构造函数，还有两个取随机的函数，build_phase里有uvm_config_db get。run_phase有raise_objection和drop_objection，用来控制仿真技术时间

--- tb_classes/random_test.svh
+++ tb_classes/random_test.svh
@@ -16,34 +16,21 @@
 class random_test extends uvm_test;
    `uvm_component_utils(random_test);
 
- virtual tinyalu_bfm bfm;
+   random_tester tester_h;
+   coverage      coverage_h;
+   scoreboard    scoreboard_h;
+
+   function void build_phase(uvm_phase phase);
+      tester_h      = new("tester_h", this);
+      coverage_h    = new("coverage_h",    this);
+      scoreboard_h  = new("scoreboard_h",    this);
+   endfunction : build_phase
    
    function new (string name, uvm_component parent);
       super.new(name,parent);
-      if(!uvm_config_db #(virtual tinyalu_bfm)::get(null, "*","bfm", bfm))
-        $fatal("Failed to get BFM");
    endfunction : new
 
-
-   task run_phase(uvm_phase phase);
-      random_tester    random_tester_h;
-      coverage  coverage_h;
-      scoreboard scoreboard_h;
-
-      phase.raise_objection(this);
-
-      random_tester_h    = new(bfm);
-      coverage_h  = new(bfm);
-      scoreboard_h = new(bfm);
-      
-      fork
-         coverage_h.execute();
-         scoreboard_h.execute();
-      join_none
-
-      random_tester_h.execute();
-      phase.drop_objection(this);
-   endtask : run_phase
-
 endclass
+   
+   
    
