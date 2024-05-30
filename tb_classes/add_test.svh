30eacb74c4cddb6d590c8914305f9c3cfa35bff9
SteveYaoLin <yaolin0616@163.com>
2024-05-30 22:24:30 +0800

    add_tester继承自random_tester，构造函数，与父类相同的函数不用覆盖，声明并覆盖get_op函数。add_test继承自random_test，如果子类继承了父类的 build_phase 和 run_phase 方法，并且子类不需要修改这些方法的行为，可以不再声明

--- tb_classes/add_test.svh
+++ tb_classes/add_test.svh
@@ -13,37 +13,21 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-class add_test extends uvm_test;
+class add_test extends random_test;
    `uvm_component_utils(add_test);
 
-   virtual tinyalu_bfm bfm;
-   
+   add_tester tester_h;
+
    function new (string name, uvm_component parent);
       super.new(name,parent);
-      if(!uvm_config_db #(virtual tinyalu_bfm)::get(null, "*","bfm", bfm))
-        $fatal("Failed to get BFM");
    endfunction : new
-   
 
-   task run_phase(uvm_phase phase);
-      add_tester   add_tester_h;
-      coverage  coverage_h;
-      scoreboard scoreboard_h;
-
-      phase.raise_objection(this);
-
-      add_tester_h = new(bfm);
-      coverage_h   = new(bfm);
-      scoreboard_h   = new(bfm);
-      
-      fork
-         coverage_h.execute();
-         scoreboard_h.execute();
-      join_none
-
-      add_tester_h.execute();
-      phase.drop_objection(this);
-   endtask : run_phase
+   function void build_phase(uvm_phase phase);
+      tester_h      = new("tester_h", this);
+      coverage_h    = new("coverage_h",    this);
+      scoreboard_h  = new("scoreboard_h",    this);
+   endfunction
 
 endclass
-   
+
+
--- tb_classes/add_tester.svh
+++ tb_classes/add_tester.svh
@@ -14,9 +14,10 @@
    limitations under the License.
 */
 class add_tester extends random_tester;
-
-   function new (virtual tinyalu_bfm b);
-      super.new(b);
+   `uvm_component_utils(add_tester)
+         
+   function new (string name, uvm_component parent);
+      super.new(name, parent);
    endfunction : new
 
    function operation_t get_op();
