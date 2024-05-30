4d4d5d353cd2b28b3baf4639454e9f5dbfa80330
SteveYaoLin <yaolin0616@163.com>
2024-05-30 23:30:31 +0800

    添加base_tester，run_phase在此声明。random_tester继承自base_tester不再需要声明run_phase。

--- tb_classes/random_tester.svh
+++ tb_classes/random_tester.svh
@@ -1,28 +1,33 @@
 /*
- Copyright 2013 Ray Salemi
+   Copyright 2013 Ray Salemi
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+       http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-class random_tester extends uvm_component;
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+class random_tester extends base_tester;
    `uvm_component_utils (random_tester)
-
-   virtual tinyalu_bfm bfm;
-
-   function new (string name, uvm_component parent);
-      super.new(name, parent);
-   endfunction : new
    
-   virtual function operation_t get_op();
+   function byte get_data();
+      bit [1:0] zero_ones;
+      zero_ones = $random;
+      if (zero_ones == 2'b00)
+        return 8'h00;
+      else if (zero_ones == 2'b11)
+        return 8'hFF;
+      else
+        return $random;
+   endfunction : get_data
+
+   function operation_t get_op();
       bit [2:0] op_choice;
       op_choice = $random;
       case (op_choice)
@@ -37,41 +42,10 @@ class random_tester extends uvm_component;
       endcase // case (op_choice)
    endfunction : get_op
 
-   virtual      function byte get_data();
-      bit [1:0] zero_ones;
-      zero_ones = $random;
-      if (zero_ones == 2'b00)
-        return 8'h00;
-      else if (zero_ones == 2'b11)
-        return 8'hFF;
-      else
-        return $random;
-   endfunction : get_data
-   
-   function void build_phase(uvm_phase phase);
-      if(!uvm_config_db #(virtual tinyalu_bfm)::get(null, "*","bfm", bfm))
-        $fatal("Failed to get BFM");
-   endfunction : build_phase
+   function new (string name, uvm_component parent);
+      super.new(name, parent);
+   endfunction : new
 
-   task run_phase(uvm_phase phase);
-      byte         unsigned        iA;
-      byte         unsigned        iB;
-      operation_t                  op_set;
-      shortint     result;
-      
-      phase.raise_objection(this);
-      bfm.reset_alu();
-      repeat (1000) begin : random_loop
-         op_set = get_op();
-         iA = get_data();
-         iB = get_data();
-         bfm.send_op(iA, iB, op_set, result);
-      end : random_loop
-      #500;
-      phase.drop_objection(this);
-   endtask : run_phase
-   
-   
 endclass : random_tester
 
 
