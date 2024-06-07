3be2ebc220b872cac8d84737c03589715b650eb8
SteveYaoLin <yaolin0616@163.com>
2024-06-07 21:01:13 +0800

    class scoreboard接受两个数据，所以要在在此建一个继承uvm_subscriber #(shortint)接受数据的基础上，加uvm_tlm_analysis_fifo #(command_s) cmd_f。然后build_phase。并在方法write里用do while try_get（cmd_f.try_get(cmd)）来去除fifo里的数据。cmd_monitor always 块调用 write_to_monitor()的时候其实是在调用所在线程内所有 订阅者的 write()

--- tb_classes/scoreboard.svh
+++ tb_classes/scoreboard.svh
@@ -13,39 +13,43 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-class scoreboard extends uvm_component;
+class scoreboard extends uvm_subscriber #(shortint);
    `uvm_component_utils(scoreboard);
 
-   virtual tinyalu_bfm bfm;
+   uvm_tlm_analysis_fifo #(command_s) cmd_f;
 
    function new (string name, uvm_component parent);
       super.new(name, parent);
    endfunction : new
-
+   
    function void build_phase(uvm_phase phase);
-      if(!uvm_config_db #(virtual tinyalu_bfm)::get(null, "*","bfm", bfm))
-        $fatal("Failed to get BFM");
+      cmd_f = new ("cmd_f", this);
    endfunction : build_phase
 
-   task run_phase(uvm_phase phase);
+   function void write(shortint t);
       shortint predicted_result;
-      ;
-      forever begin : self_checker
-         @(posedge bfm.done)
-	   #1;
-           case (bfm.op_set)
-             add_op: predicted_result = bfm.A + bfm.B;
-             and_op: predicted_result = bfm.A & bfm.B;
-             xor_op: predicted_result = bfm.A ^ bfm.B;
-             mul_op: predicted_result = bfm.A * bfm.B;
-           endcase // case (op_set)
-         
-         if ((bfm.op_set != no_op) && (bfm.op_set != rst_op))
-           if (predicted_result != bfm.result)
-             $error ("FAILED: A: %0h  B: %0h  op: %s result: %0h",
-                     bfm.A, bfm.B, bfm.op_set.name(), bfm.result);
-      end : self_checker
-   endtask : run_phase
+      command_s cmd; 
+      cmd.op = no_op;
+      do
+       if (!cmd_f.try_get(cmd)) $fatal(1, "No command in self checker");
+      while ((cmd.op == no_op) || (cmd.op == rst_op));
+      
+      case (cmd.op)
+        add_op: predicted_result = cmd.A + cmd.B;
+        and_op: predicted_result = cmd.A & cmd.B;
+        xor_op: predicted_result = cmd.A ^ cmd.B;
+        mul_op: predicted_result = cmd.A * cmd.B;
+      endcase // case (op_set)
+
+      if (predicted_result != t)
+       $error (
+     "FAILED: A: %2h  B: %2h  op: %s actual result: %4h   expected: %4h",
+         cmd.A, cmd.B, cmd.op.name(), t,  predicted_result);
+   endfunction : write         
+
+
+
+
 endclass : scoreboard
 
 
