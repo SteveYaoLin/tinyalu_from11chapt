b7e2accac61188c65a8696cdef5d2ac23a818738
SteveYaoLin <yaolin0616@163.com>
2024-06-07 16:07:13 +0800

    bfm句柄里包括tinyalu_pkg，还包括了command_monitor_h和result_monitor_h新句柄。把rst_monitor和rslt_monitor提出来，与cmd_monitor一起，一直作用。

--- tinyalu_bfm.sv
+++ tinyalu_bfm.sv
@@ -15,6 +15,9 @@
 */
 interface tinyalu_bfm;
    import tinyalu_pkg::*;
+   
+   command_monitor command_monitor_h;
+   result_monitor  result_monitor_h;
 
    byte         unsigned        A;
    byte         unsigned        B;
@@ -26,6 +29,9 @@ interface tinyalu_bfm;
    wire [15:0]  result;
    operation_t  op_set;
 
+
+   
+   
    assign op = op_set;
 
    task reset_alu();
@@ -38,10 +44,8 @@ interface tinyalu_bfm;
    
 
 
-   task send_op(input byte iA, input byte iB, input operation_t iop, shortint alu_result);
+   task send_op(input byte iA, input byte iB, input operation_t iop, shortint result);
       if (iop == rst_op) begin
-         @(negedge clk);
-         op_set = iop;
          @(posedge clk);
          reset_n = 1'b0;
          start = 1'b0;
@@ -62,12 +66,35 @@ interface tinyalu_bfm;
             do
               @(negedge clk);
             while (done == 0);
-            alu_result = result;
             start = 1'b0;
          end
       end // else: !if(iop == rst_op)
       
    endtask : send_op
+
+
+   always @(posedge clk) begin : cmd_monitor
+      bit new_command;
+      if (!start) 
+        new_command = 1;
+      else
+        if (new_command) begin 
+           command_monitor_h.write_to_monitor(A, B, op);
+           new_command = (op == 3'b000); // handle no_op
+        end 
+   end : cmd_monitor
+
+   always @(negedge reset_n) begin : rst_monitor
+      if (command_monitor_h != null) //guard against VCS time 0 negedge
+         command_monitor_h.write_to_monitor(A, B, rst_op);
+   end : rst_monitor
+   
+
+   always @(posedge clk) begin : rslt_monitor
+         if (done) 
+           result_monitor_h.write_to_monitor(result);
+   end : rslt_monitor
+   
 
    initial begin
       clk = 0;
@@ -79,5 +106,3 @@ interface tinyalu_bfm;
 
 
 endinterface : tinyalu_bfm
-
-   
